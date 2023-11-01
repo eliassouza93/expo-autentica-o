f18774137121fbbca52b70ad5cbf4ce0d42e2de3
Elias de Souza <elias99-souza@hotmail.com>
2023-11-01 15:53:50 -0300

    autenticações

---
+++ src/componentes/Alerta/index.js
@@ -0,0 +1,17 @@
+import { Snackbar } from "react-native-paper";
+
+export function Alerta({ mensagem, error = false, setError }) {
+    return (
+        <Snackbar
+            visible={error}
+            onDismiss={() => setError(false)}
+            duration={1500}
+            action={{
+                label: 'Ok',
+                onPress: () => setError(false)
+            }}
+        >
+            {mensagem}
+        </Snackbar>
+    )
+}
