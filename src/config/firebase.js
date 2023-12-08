f6e9ffe3ead585e1158a16d9e98fc5d5a4ac512b
Elias de Souza <92374016+eliassouza93@users.noreply.github.com>
2023-12-08 14:30:11 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,13 +6,7 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth/react-n
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAYSllmLyWThzkcsMgiIuCTYaYQ1bDKYcE",
-    authDomain: "esporte-4e3d9.firebaseapp.com",
-    projectId: "esporte-4e3d9",
-    storageBucket: "esporte-4e3d9.appspot.com",
-    messagingSenderId: "811814096998",
-    appId: "1:811814096998:web:e93d77752a74d11d8b9da7",
-    measurementId: "G-VVJWEDZ37B"
+    '..'
 };
 
 const app = initializeApp(firebaseConfig);
